3090beeafe7dd144ab1fb6a78953eac6a2ab47d1
Vladimir <61161775+VishovVladimir@users.noreply.github.com>
2023-12-22 01:48:19 +0400

    Update mnist_loader.py

--- src/mnist_loader.py
+++ src/mnist_loader.py
@@ -40,7 +40,7 @@ def load_data():
     below.
     """
     f = gzip.open('../data/mnist.pkl.gz', 'rb')
-    training_data, validation_data, test_data = pickle.load(f, encoding='latin1')
+    training_data, validation_data, test_data = pickle.load(f,encoding='iso-8859-1')
     f.close()
     return (training_data, validation_data, test_data)
 
